40786a6f4200a7982f668afbfd0b56227f3358fe
Kate Natochii <katyakatya2736@gmail.com>
2020-06-24 19:47:31 +0300

    start with social media (divide into components)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Technologies from './Technologies'
-import Header from './Header'
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 // function App() {
 const App = () => {
   return (
-    <div>
+    <div className= "wrapper">
       <Header />
-      <Technologies />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+      <header className= "header">
+        <div className= "header__logo">LOGO</div>
+      </header>
+   )
+}
+
+export default Header;
